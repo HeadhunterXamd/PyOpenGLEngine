7e140be07be5d16486018ef9fc237355cbb053da
Niels van Schooten <nielsvanschooten@gmail.com>
2015-01-25 14:09:39 +0100

    commented out some redundant code

--- PyGEF/Renderer/glRender.py
+++ PyGEF/Renderer/glRender.py
@@ -50,30 +50,32 @@ def render():
 	glutSwapBuffers()
 
 
-class renderer():
-	def __init__(self, width, height):
-		# A general OpenGL initialization function.  Sets all of the initial parameters.
-		glClearColor(0.0, 0.0, 0.0, 0.0)  # This Will Clear The Background Color To Black
-		glClearDepth(1.0)  # Enables Clearing Of The Depth Buffer
-		glDepthFunc(GL_LESS)  # The Type Of Depth Test To Do
-		glEnable(GL_DEPTH_TEST)  # Enables Depth Testing
-		glShadeModel(GL_SMOOTH)  # Enables Smooth Color Shading
+### causes an null reference error for the glContext.
 
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()  # Reset The Projection Matrix
-		# Calculate The Aspect Ratio Of The Window
-		gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
-
-		glMatrixMode(GL_MODELVIEW)
-
-
-	def render(self, *args):
-		if len(args) > 0:
-			return
-		# test
-		# test2
-		else:
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # // Clear Screen And Depth Buffer
-			glLoadIdentity()  # // Reset The Current Modelview Matrix
-
-			glutSwapBuffers()
+# class renderer():
+# 	def __init__(self, width, height):
+# 		# A general OpenGL initialization function.  Sets all of the initial parameters.
+# 		glClearColor(0.0, 0.0, 0.0, 0.0)  # This Will Clear The Background Color To Black
+# 		glClearDepth(1.0)  # Enables Clearing Of The Depth Buffer
+# 		glDepthFunc(GL_LESS)  # The Type Of Depth Test To Do
+# 		glEnable(GL_DEPTH_TEST)  # Enables Depth Testing
+# 		glShadeModel(GL_SMOOTH)  # Enables Smooth Color Shading
+#
+# 		glMatrixMode(GL_PROJECTION)
+# 		glLoadIdentity()  # Reset The Projection Matrix
+# 		# Calculate The Aspect Ratio Of The Window
+# 		gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
+#
+# 		glMatrixMode(GL_MODELVIEW)
+#
+#
+# 	def render(self, *args):
+# 		if len(args) > 0:
+# 			return
+# 		# test
+# 		# test2
+# 		else:
+# 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # // Clear Screen And Depth Buffer
+# 			glLoadIdentity()  # // Reset The Current Modelview Matrix
+#
+# 			glutSwapBuffers()
